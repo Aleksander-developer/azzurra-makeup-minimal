b62b2c980c28538e688078ad310435bebf2cd900
labforweb <aleksandernikollibusiness@gmail.com>
2025-08-03 11:11:23 +0200

    fix(SSR): Add NoopAnimationsModule to server module

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,5 +1,9 @@
+// src/app/app.server.module.ts
+
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
+// ---> 1. IMPORTA QUESTO MODULO <---
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
@@ -8,6 +12,10 @@ import { AppComponent } from './app.component';
   imports: [
     AppModule,
     ServerModule,
+    // ---> 2. AGGIUNGI QUESTO MODULO <---
+    // Questa è la riga che risolve il crash.
+    // Sostituisce BrowserAnimationsModule con una versione "innocua" sul server.
+    NoopAnimationsModule
   ],
   bootstrap: [AppComponent],
 })
